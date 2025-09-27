5ba0cc82d2530f41fce782656f93552d67869680
rodrigo1601 <ribanez0116@gmail.com>
2025-09-27 17:45:09 -0300

    Update: cambios ligeros

--- fs-ejemplo/index.mjs
+++ fs-ejemplo/index.mjs
@@ -1,18 +1,18 @@
 import fs from 'fs';
 
 //leer un archivo de manera asincrona
-fs.readFile('./example.txt', 'utf8', (err, data) => {
+fs.readFile('./data/example.txt', 'utf8', (err, data) => {
     if (err) throw err;
     console.log('Contenido del archivo:', data);
 });
 
 //escribir un archivo nuevo
-fs.writeFile('./newFile.txt', 'Contenido nuevo', (err) => {
+fs.writeFile('./data/newFile.txt', 'Contenido nuevo', (err) => {
     if (err) throw err;
     console.log('Archivo creado y escrito');
 
     //renombrar el archivo creado
-    fs.rename('./newFile.txt', './renamedFile.txt', (err) => {
+    fs.rename('./data/newFile.txt', './data/renamedFile.txt', (err) => {
         if (err) throw err;
         console.log('Archivo renombrado');
     });
